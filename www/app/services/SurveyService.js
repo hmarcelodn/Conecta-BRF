b88b16a1fddbec87a801ba32387baf587be6a32b
Jimena Gauto <jimenag@lagash.com>
2016-01-25 02:30:20 -0300

    Survey Service

---
+++ www/app/services/SurveyService.js
@@ -0,0 +1,22 @@
+brfPhoneGapApp.factory('surveyService', ['$http', '$q', function($http, $q){
+	return {
+		setSurvey: function(){
+
+		},
+		setNoBrf: function(){
+
+		},
+		setQuestion: function(){
+
+		},
+		getSurvey: function(){
+
+		},
+		getNoBrf: function(){
+
+		},
+		getQuestions: function(){
+			
+		}
+	}
+}]);
