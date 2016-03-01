b41d4c0e1196c4ffc4823351554623440727352f
Jimena Gauto <jimenag@lagash.com>
2016-03-01 12:08:41 -0300

    Redirect To Dashboard

--- www/app/controllers/DashboardController.js
+++ www/app/controllers/DashboardController.js
@@ -8,11 +8,8 @@ brfPhoneGapApp.controller('dashboardController', function($scope, $route, Dashbo
     
     Survey.getVisitedCoachingPdvsCount(Login.getToken().id)
         .then(function (result) {
-            console.log("Resultados:");
-            console.log(result);
             $scope.visitedPDVs = (result === undefined ? 0 : result);
- 
-             
+              
              Dashboard.getDailyBase(Login.getToken().id)
                 .then(function (base) {
                     $scope.obtainedCompliance = Math.round((result / base.target_coaching) * 100);
@@ -22,19 +19,12 @@ brfPhoneGapApp.controller('dashboardController', function($scope, $route, Dashbo
         });
         
 	Survey.getAveragePerModule($routeParams.auditId)
-        .then(function (values) {
-            
-            console.log("test 1");
-            console.log(values);
+        .then(function (values) {           
                         
             angular.forEach(values, function (value, key) {
-                console.log($routeParams.auditId);
                 
                 Question.getAuditedQuestionsResume(value.id_mod, $routeParams.auditId)
-                    .then(function (questionsResult1) {
-
-                        console.log("test 2");
-                        console.log(questionsResult1);                        
+                    .then(function (questionsResult1) {                      
 
                         $scope.modules.push
                         ( {
