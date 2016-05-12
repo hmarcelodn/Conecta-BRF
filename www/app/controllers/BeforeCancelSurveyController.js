b0ebb9977300e609f3bcb8594e0047ebe2eeea9b
Hugo Marcelo Del Negro <hdelnegro@belatrix.com>
2016-05-12 00:23:50 -0300

    Adding before cancel audit functionality

---
+++ www/app/controllers/BeforeCancelSurveyController.js
@@ -0,0 +1,19 @@
+(function() {
+'use strict';
+
+    angular
+        .module('brfPhoneGapApp')
+        .controller('BeforeCancelSurveyController', BeforeCancelSurveyController);
+
+    BeforeCancelSurveyController.$inject = [];
+    function BeforeCancelSurveyController() {
+        var vm = this;      
+        
+        activate();
+
+        ////////////////
+
+        function activate() { }        
+       
+    }
+})();
