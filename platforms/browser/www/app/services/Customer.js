33441dee15e68e44a91ed809c0c200859ab2bb9c
CorrecaminosAr <lperezpastorino@gmail.com>
2016-07-28 00:18:09 -0300

    Build_20160728_0008_v145_Sincro

--- platforms/browser/www/app/services/Customer.js
+++ platforms/browser/www/app/services/Customer.js
@@ -1,66 +1,88 @@
 (function() {
-'use strict';
+    'use strict';
 
     angular
         .module('brfPhoneGapApp')
         .factory('Customer', Customer);
 
-    Customer.$inject = ['$http', 'Database'];
-    function Customer($http, Database) {
-        
-        var synchronizeCustomers = function (){
+    Customer.$inject = ['$http', 'Database', '$rootScope', 'Login'];
+
+    function Customer($http, Database, $rootScope, Login) {
+        //LUUU crear 3 synchronizeCustomers 
+
+        var synchronizeCustomers = function() {
             return $http.get('https://ws.conecta-brf.com/get/customers/?token=560a100abad225d5afdf4fc6e5334917');
         };
 
-        var synchronizeCustomerTypes = function (argument) {
+        var synchronizeCustomersAll = function() {
+            return $http.get('https://ws.conecta-brf.com/v2/get/customers/?token=560a100abad225d5afdf4fc6e5334917&customer_type=3&selected_ids=&id_user=' + Login.getToken().id);
+        };
+
+        var synchronizeCustomersChains = function() {
+            //console.log ('def chains');
+            //console.log('https://ws.conecta-brf.com/v2/get/customers/?token=560a100abad225d5afdf4fc6e5334917&customer_type=1&selected_ids=' + $rootScope.SelectedChains + '&id_user=' + Login.getToken().id);
+            return $http.get('https://ws.conecta-brf.com/v2/get/customers/?token=560a100abad225d5afdf4fc6e5334917&customer_type=1&selected_ids=' + $rootScope.SelectedChains + '&id_user=' + Login.getToken().id);
+        };
+
+        var synchronizeCustomersDis = function() {
+            console.log('def chains');
+            console.log('https://ws.conecta-brf.com/v2/get/customers/?token=560a100abad225d5afdf4fc6e5334917&customer_type=2&selected_ids=' + $rootScope.SelectedDis + '&id_user=' + Login.getToken().id);
+            return $http.get('https://ws.conecta-brf.com/v2/get/customers/?token=560a100abad225d5afdf4fc6e5334917&customer_type=2&selected_ids=' + $rootScope.SelectedDis + '&id_user=' + Login.getToken().id);
+        };
+
+
+        var synchronizeCustomerTypes = function(argument) {
             return $http.get('https://ws.conecta-brf.com/get/customers/type/?token=560a100abad225d5afdf4fc6e5334917');
         };
 
-        var setCustomer = function (id, companyName, cuit, address, pdvType, highlighted, channelid, code) {
+        var setCustomer = function(id, companyName, cuit, address, pdvType, highlighted, channelid, code) {
             return Database.query('INSERT INTO Customer(customerId, companyName, cuit, address, pdvType, highlighted, channelId, code) VALUES(?, ?, ?, ?, ?, ?, ?, ?)', [id, companyName, cuit, address, pdvType, highlighted, channelid, code])
-                .then(function (result){
+                .then(function(result) {
                     return true;
                 });
         };
 
-        var setCustomerType = function (id, name) {
+        var setCustomerType = function(id, name) {
             return Database.query('INSERT INTO CustomerType(id, name) VALUES(?, ?)', [id, name])
-                .then(function (result){
+                .then(function(result) {
                     return true;
-                });			
+                });
         };
 
-        var getCustomers = function (channelid){
+        var getCustomers = function(channelid) {
             return Database.query('SELECT customerId, address, highlighted, channelId, code FROM Customer WHERE channelId=? LIMIT 0, 2000', [channelid])
-                .then(function (result){
+                .then(function(result) {
                     return Database.fetchAll(result);
                 });
         };
 
-        var getCustomerTypes = function (){
+        var getCustomerTypes = function() {
             return Database.query('SELECT id, name FROM CustomerType')
-                .then(function (result){
+                .then(function(result) {
                     return Database.fetchAll(result);
                 });
-        };        
-        
-        var getPdvTypeByCustomerId = function (customerId) {
-            return Database.query('SELECT pdvType FROM Customer')
-                .then(function (result) {
-                   return Database.fetch(result); 
-                });
         };
-        
-        var getPdvById = function (customerId) {
-            return Database.query('SELECT c.customerId, c.companyName, c.cuit, c.address, c.pdvType, c.highlighted, c.channelId, c.code, ct.name as pdvTypeName '+
-                                  'FROM Customer c INNER JOIN CustomerType ct ON c.pdvType = ct.id WHERE CustomerId = ? ', [customerId])
-                .then(function (result) {
+
+        var getPdvTypeByCustomerId = function(customerId) {
+            return Database.query('SELECT pdvType FROM Customer')
+                .then(function(result) {
                     return Database.fetch(result);
                 });
         };
-        
+
+        var getPdvById = function(customerId) {
+            return Database.query('SELECT c.customerId, c.companyName, c.cuit, c.address, c.pdvType, c.highlighted, c.channelId, c.code, ct.name as pdvTypeName ' +
+                    'FROM Customer c INNER JOIN CustomerType ct ON c.pdvType = ct.id WHERE CustomerId = ? ', [customerId])
+                .then(function(result) {
+                    return Database.fetch(result);
+                });
+        };
+
         var service = {
-            synchronizeCustomers:synchronizeCustomers,
+            synchronizeCustomers: synchronizeCustomers,
+            synchronizeCustomersAll: synchronizeCustomersAll,
+            synchronizeCustomersChains: synchronizeCustomersChains,
+            synchronizeCustomersDis: synchronizeCustomersDis,
             synchronizeCustomerTypes: synchronizeCustomerTypes,
             setCustomer: setCustomer,
             setCustomerType: setCustomerType,
@@ -69,7 +91,7 @@
             getPdvTypeByCustomerId: getPdvTypeByCustomerId,
             getPdvById: getPdvById
         };
-        
+
         return service;
     }
 })();
--- www/app/routes.js
+++ www/app/routes.js
@@ -1,263 +1,272 @@
-brfPhoneGapApp.config(['$routeProvider', function($routeProvider){
-	$routeProvider
-		.when('/Main', {
-			templateUrl: 'app/views/main.html',
-			controller: 'MainController',
+brfPhoneGapApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/Main', {
+            templateUrl: 'app/views/main.html',
+            controller: 'MainController',
             controllerAs: 'vm',
-			access:{
-				isFreeAccess: false,
-				audit: false
-			}
-		})
-		.when('/Dashboard/:auditId', {
-			templateUrl: 'app/views/dashboard.html',
-			controller: 'DashboardController',
-			access:{
-				isFreeAccess: false,
-				audit: false
-			}
-		})
-		.when('/Audit/:auditId', {
-			templateUrl: 'app/views/welcome.html',
-			controller: 'WelcomeController',
-            controllerAs: 'vm',
-			access:{
-				isFreeAccess: false,
-				audit: false
-			}			
-		})
-		.when('/Audit/:auditId/Channel', {
-			templateUrl: 'app/views/channels.html',
-			controller: 'ChannelsController',
-            controllerAs: 'vm',
-			access:{
-				isFreeAccess: false,
-				audit: false
-			}
-		})
-		.when('/Audit/:auditId/Channel/:channelId/Pdv', {
-			templateUrl: 'app/views/form.html',
-			controller: 'FormController',
-            controllerAs: 'vm',
-			access:{
-				isFreeAccess: false,
-				audit: false
-			}
-		})
-		.when('/Audit/:auditId/Channel/:channelId/AddPdv', {
-			templateUrl: 'app/views/new-pdv.html',
-			controller: 'PdvController',
-			controllerAs: 'vm',
-			access:{
-				isFreeAccess: false,
-				audit: false
-			}
-		})
-		.when('/Audit/:auditId/Channel/:channelId/Pdv/:pdvId/Seller', {
-			templateUrl: 'app/views/search.html',
-			controller: 'SearchController',
-            controllerAs: 'vm',
-			access:{
-				isFreeAccess: false,
-				audit: false
-			}	
-		})
-		.when('/',{
-			templateUrl: 'app/views/login.html',
-			controller: 'AuthController',
-            controllerAs: 'vm',
-			access:{
-				isFreeAccess: true				
-			}
-		})
-		.when('/Synchronizer/:syncModeId', {
-			templateUrl: 'app/views/synchronizer.html',
-			controller: 'SynchronizerController',
-			access:{
-				isFreeAccess: false,
-				audit: false
-			}
-		})
-		.when('/DoSynchronization', {
-			templateUrl: 'app/views/doSynchronization.html',
-			controller: 'DoSynchronizationController',
-            controllerAs: 'vm',
-			access:{
-				isFreeAccess: false,
-				audit: false
-			}
-		})
-        .when('/SendSynchronization',{
-			templateUrl: 'app/views/sendSynchronization.html',
-			controller: 'SendSynchronizationController',
-            controllerAs: 'vm',
-			access:{
-				isFreeAccess: false,
-				audit: false
-			}            
+            access: {
+                isFreeAccess: false,
+                audit: false
+            }
         })
-		.when('/SyncOk/:syncId', {
-			templateUrl: 'app/views/syncOk.html',
-			controller: 'SyncOkController',
+        .when('/Dashboard/:auditId', {
+            templateUrl: 'app/views/dashboard.html',
+            controller: 'DashboardController',
+            access: {
+                isFreeAccess: false,
+                audit: false
+            }
+        })
+        .when('/Audit/:auditId', {
+            templateUrl: 'app/views/welcome.html',
+            controller: 'WelcomeController',
             controllerAs: 'vm',
-			access:{
-				isFreeAccess: false,
-				audit: false
-			}
-		})
-		.when('/SyncNok', {
-			templateUrl: 'app/views/syncNok.html',
-			controller: 'SyncNokController',
+            access: {
+                isFreeAccess: false,
+                audit: false
+            }
+        })
+        .when('/Audit/:auditId/Channel', {
+            templateUrl: 'app/views/channels.html',
+            controller: 'ChannelsController',
             controllerAs: 'vm',
-			access:{
-				isFreeAccess: false,
-				audit: false
-			}
-		})		
-		.when('/Audit/:auditId/Channel/:channelId/Pdv/:pdvId/Seller/:sellerId/observaciones', {
-			templateUrl: 'app/views/observations.html',
-			controller: 'ObservationsController',
-			access:{
-				isFreeAccess: false,
-				audit: true
-			}
-		})
-		.when('/Audit/:auditId/Channel/:channelId/Pdv/:pdvId/Seller/:sellerId/observaciones_rtm', {
-			templateUrl: 'app/views/observations.html',
-			controller: 'ObservationsController',
-			access:{
-				isFreeAccess: false,
-				audit: true
-			}
-		})		
-		.when('/Audit/:auditId/Channel/:channelId/Pdv/:pdvId/Seller/:sellerId/no_brf', {
-			templateUrl: 'app/views/noBrf.html',
-			controller: 'NoBrfController',
+            access: {
+                isFreeAccess: false,
+                audit: false
+            }
+        })
+        .when('/Audit/:auditId/Channel/:channelId/Pdv', {
+            templateUrl: 'app/views/form.html',
+            controller: 'FormController',
             controllerAs: 'vm',
-			access:{
-				isFreeAccess: false,
-				audit: true
-			}
-		})
-        .when('/Audit/:auditId/Channel/:channelId/Pdv/:pdvId/Seller/:sellerId/Mandatory',{
-            templateUrl: 'app/views/mandatoryQuestions.html',
-            controller: 'MandatoryQuestionsController',
+            access: {
+                isFreeAccess: false,
+                audit: false
+            }
+        })
+        .when('/Audit/:auditId/Channel/:channelId/AddPdv', {
+            templateUrl: 'app/views/new-pdv.html',
+            controller: 'PdvController',
             controllerAs: 'vm',
-            access:{
+            access: {
+                isFreeAccess: false,
+                audit: false
+            }
+        })
+        .when('/Audit/:auditId/Channel/:channelId/Pdv/:pdvId/Seller', {
+            templateUrl: 'app/views/search.html',
+            controller: 'SearchController',
+            controllerAs: 'vm',
+            access: {
+                isFreeAccess: false,
+                audit: false
+            }
+        })
+        .when('/', {
+            templateUrl: 'app/views/login.html',
+            controller: 'AuthController',
+            controllerAs: 'vm',
+            access: {
+                isFreeAccess: true
+            }
+        })
+        .when('/Synchronizer/:syncModeId', {
+            templateUrl: 'app/views/synchronizer.html',
+            controller: 'SynchronizerController',
+            access: {
+                isFreeAccess: false,
+                audit: false
+            }
+        })
+        .when('/DoSynchronization', {
+            templateUrl: 'app/views/doSynchronization.html',
+            controller: 'DoSynchronizationController',
+            controllerAs: 'vm',
+            access: {
+                isFreeAccess: false,
+                audit: false
+            }
+        })
+        .when('/SendSynchronization', {
+            templateUrl: 'app/views/sendSynchronization.html',
+            controller: 'SendSynchronizationController',
+            controllerAs: 'vm',
+            access: {
+                isFreeAccess: false,
+                audit: false
+            }
+        })
+        .when('/SyncOk/:syncId', {
+            templateUrl: 'app/views/syncOk.html',
+            controller: 'SyncOkController',
+            controllerAs: 'vm',
+            access: {
+                isFreeAccess: false,
+                audit: false
+            }
+        })
+        .when('/SyncNok', {
+            templateUrl: 'app/views/syncNok.html',
+            controller: 'SyncNokController',
+            controllerAs: 'vm',
+            access: {
+                isFreeAccess: false,
+                audit: false
+            }
+        })
+        .when('/Audit/:auditId/Channel/:channelId/Pdv/:pdvId/Seller/:sellerId/observaciones', {
+            templateUrl: 'app/views/observations.html',
+            controller: 'ObservationsController',
+            access: {
                 isFreeAccess: false,
                 audit: true
             }
-        })        
-		.when('/Audit/:auditId/Channel/:channelId/Pdv/:pdvId/Seller/:sellerId/Module/:moduleId', {
-			templateUrl: 'app/views/questions.html',
-			controller: 'QuestionsController',
+        })
+        .when('/Audit/:auditId/Channel/:channelId/Pdv/:pdvId/Seller/:sellerId/observaciones_rtm', {
+            templateUrl: 'app/views/observations.html',
+            controller: 'ObservationsController',
+            access: {
+                isFreeAccess: false,
+                audit: true
+            }
+        })
+        .when('/Audit/:auditId/Channel/:channelId/Pdv/:pdvId/Seller/:sellerId/no_brf', {
+            templateUrl: 'app/views/noBrf.html',
+            controller: 'NoBrfController',
             controllerAs: 'vm',
-			access: {
-				isFreeAccess: false,
-				audit: true
-			}
-		})
-		.when('/Audit/:auditId/Channel/:channelId/Pdv/:pdvId/Seller/:sellerId/Module/:moduleId/Category/:categoryId/CategoryType/:categoryType', {
-			templateUrl: 'app/views/questions.html',
-			controller: 'QuestionsController',
+            access: {
+                isFreeAccess: false,
+                audit: true
+            }
+        })
+        .when('/Audit/:auditId/Channel/:channelId/Pdv/:pdvId/Seller/:sellerId/Mandatory', {
+            templateUrl: 'app/views/mandatoryQuestions.html',
+            controller: 'MandatoryQuestionsController',
             controllerAs: 'vm',
-			access: {
-				isFreeAccess: false,
-				audit: true
-			}
-		})		
-		.when('/Audit/:auditId/Channel/:channelId/Pdv/:pdvId/Seller/:sellerId/:slug/:default?', {
-			templateUrl: 'app/views/categorySearch.html',
-			controller: 'CategoryController',
+            access: {
+                isFreeAccess: false,
+                audit: true
+            }
+        })
+        .when('/Audit/:auditId/Channel/:channelId/Pdv/:pdvId/Seller/:sellerId/Module/:moduleId', {
+            templateUrl: 'app/views/questions.html',
+            controller: 'QuestionsController',
             controllerAs: 'vm',
-			access:{
-				isFreeAccess: false,
-				audit: true
-			}
-		})			
-        .when('/Help/:questionId',{
+            access: {
+                isFreeAccess: false,
+                audit: true
+            }
+        })
+        .when('/Audit/:auditId/Channel/:channelId/Pdv/:pdvId/Seller/:sellerId/Module/:moduleId/Category/:categoryId/CategoryType/:categoryType', {
+            templateUrl: 'app/views/questions.html',
+            controller: 'QuestionsController',
+            controllerAs: 'vm',
+            access: {
+                isFreeAccess: false,
+                audit: true
+            }
+        })
+        .when('/Audit/:auditId/Channel/:channelId/Pdv/:pdvId/Seller/:sellerId/:slug/:default?', {
+            templateUrl: 'app/views/categorySearch.html',
+            controller: 'CategoryController',
+            controllerAs: 'vm',
+            access: {
+                isFreeAccess: false,
+                audit: true
+            }
+        })
+        .when('/Help/:questionId', {
             templateUrl: 'app/views/help.html',
             controller: 'HelpController',
             controllerAs: 'vm',
-            access:{
-  				isFreeAccess: false,
-				audit: true              
+            access: {
+                isFreeAccess: false,
+                audit: true
             }
         })
         .when('/WebSQL', {
             templateUrl: 'app/views/webSQL.html',
             controller: 'WebSQLController',
             controllerAs: 'vm',
-            access:{
-  				isFreeAccess: true,
-  				audit: true           
-            }            
+            access: {
+                isFreeAccess: true,
+                audit: true
+            }
         })
         .when('/beforeLogOff', {
             templateUrl: 'app/views/beforeLogOff.html',
             controller: 'BeforeLogOffController',
             controllerAs: 'vm',
-            access:{
+            access: {
                 isFreeAccess: false,
                 audit: false
             }
         })
         .when('/beforeCancelSurvey', {
-        	templateUrl: 'app/views/beforeCancelSurvey.html',
-        	controller: 'BeforeCancelSurveyController',
-        	controllerAs: 'vm',
-        	access:{
-        		isFreeAccess: true,
-        		audit: true        		
-        	}
+            templateUrl: 'app/views/beforeCancelSurvey.html',
+            controller: 'BeforeCancelSurveyController',
+            controllerAs: 'vm',
+            access: {
+                isFreeAccess: true,
+                audit: true
+            }
         })
-        .when('/LockSynchronization/:leftCoaching',{
+        .when('/LockSynchronization/:leftCoaching', {
             templateUrl: 'app/views/lockSynchronization.html',
             controller: 'LockSynchronizationController',
             controllerAs: 'vm',
-            access:{
+            access: {
                 isFreeAccess: false,
                 audit: false
             }
         })
-		.otherwise({
-			redirectTo: '/'
-		})
+        .when('/SelectPDV', {
+            templateUrl: 'app/views/SelectPDV.html',
+            controller: 'SelectPDVController',
+            controllerAs: 'vm',
+            access: {
+                isFreeAccess: false,
+                audit: false
+            }
+        })
+
+    .otherwise({
+        redirectTo: '/'
+    })
 }]).
-run(function($rootScope, $location, Login, Survey, Database, Module){
+run(function($rootScope, $location, Login, Survey, Database, Module) {
 
-	Database.init();
+    Database.init();
 
-	$rootScope.$on('$viewContentLoaded', function(event){
-		$('.collapsible').collapsible();
-		$('select').material_select();
-	});
+    $rootScope.$on('$viewContentLoaded', function(event) {
+        $('.collapsible').collapsible();
+        $('select').material_select();
+    });
 
-	$rootScope.$on('$routeChangeStart', function(currRoute, prevRoute){        
+    $rootScope.$on('$routeChangeStart', function(currRoute, prevRoute) {
 
-		if (prevRoute.access != undefined) {
+        if (prevRoute.access != undefined) {
             if (!prevRoute.access.isFreeAccess && !Login.authenticated()) {
                 $location.path('/');
             }
         }
 
-        if(prevRoute.access != undefined){
-        	 if(!prevRoute.access.audit && Survey.getAuditMode()){                
-                 
-        	 	/* Load default Module */
-	            Module.getModules(Survey.getAuditChannel(), Login.getToken().id_role, Survey.getAuditId()).then(function(modules){
-	        	 	$location.path('/Audit/' + Survey.getAuditId() +
-	                               '/Channel/' + Survey.getAuditChannel() + 
-	        	 				   '/Pdv/' + Survey.getAuditPdv() + 
-	        	 				   '/Seller/' + Survey.getAuditSeller() + 
-	        	 				   '/' + modules[0].slug + 
-	        	 				   '/defaultModule');	                
-	            });
+        if (prevRoute.access != undefined) {
+            if (!prevRoute.access.audit && Survey.getAuditMode()) {
 
-        	 }
+                /* Load default Module */
+                Module.getModules(Survey.getAuditChannel(), Login.getToken().id_role, Survey.getAuditId()).then(function(modules) {
+                    $location.path('/Audit/' + Survey.getAuditId() +
+                        '/Channel/' + Survey.getAuditChannel() +
+                        '/Pdv/' + Survey.getAuditPdv() +
+                        '/Seller/' + Survey.getAuditSeller() +
+                        '/' + modules[0].slug +
+                        '/defaultModule');
+                });
+
+            }
+        } else {
+            $location.path('/Main');
         }
-        else{
-        	$location.path('/Main');
-        }
-	});
+    });
 });
